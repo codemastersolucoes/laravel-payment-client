94ec71002d2a471a5697dc12b17e41b1e02783d6
Felipe da Silva Pinheiro <felipepinheiro@univicosa.com.br>
2018-03-16 14:58:19 -0300

    Callback added

--- Entities/DebitCard.php
+++ Entities/DebitCard.php
@@ -37,20 +37,27 @@ class DebitCard implements \JsonSerializable
     private $expiration;
 
     /**
+     * @var string
+     */
+    private $callback;
+
+    /**
      * CreditCard constructor.
      * @param string $number
      * @param string $holder
      * @param string $brand
      * @param string $cvv
      * @param string $expiration
+     * @param string $callback
      */
-    public function __construct(string $number, string $holder, string $brand, string $cvv, string $expiration)
+    public function __construct(string $number, string $holder, string $brand, string $cvv, string $expiration, string $callback)
     {
         $this->number = $number;
         $this->holder = $holder;
         $this->brand = $brand;
         $this->cvv = $cvv;
         $this->expiration = Carbon::createFromFormat('m/Y', $expiration);
+        $this->callback = $callback;
     }
 
     /**
@@ -75,7 +82,8 @@ class DebitCard implements \JsonSerializable
             'brand' => $this->brand,
             'number' => $this->number,
             'holder' => $this->holder,
-            'expiration' => $this->expiration->format('m/Y')
+            'expiration' => $this->expiration->format('m/Y'),
+            'callback' => $this->callback
         ];
 
         return array_merge(compact('debit_card'), $this->payment->jsonSerialize());
